e2c1e3ed013753d127cc7c26990a1f673ed4facd
codeboysummer <aw.wahid13@gmail.com>
2022-12-23 21:44:28 -0500

    ''

---
+++ src/data/info.ts
@@ -0,0 +1,79 @@
+export const info = {
+  baseUrl: "https://astro-portfolio-uzair.vercel.app",
+  name: "Abdul Wahid",
+  jobDescription: "Web Developer",
+  about: `I am a highly skilled web developer with experience in front-end and back-end technologies including HTML, CSS, JavaScript, TypeScript, React.js, Next.js, Chakra UI, Material UI, Axios, Java, SQL, MySQL, MongoDB, FireBase, Node.js, and Express. I have a strong track record of developing and designing successful web applications and have experience with design tools, testing libraries, WordPress, and Linux operating systems. I have developed a variety of successful side projects and hold an Associates degree in Computer Science from Algonquin College
+  `,
+
+  experience: [
+    {
+      name: "UMC, Inc",
+      location: "Toronto,Canada",
+      startDate: "2021",
+      endDate: "Present",
+      description: [
+        "✓ Developed user interfaces using React components, including Chakra UI, resulting in a 50% increase in user engagement",
+        "✓ Implemented responsive design using modern CSS frameworks such as SCSS, Sass, and Tailwind, ensuring a consistent user experience across all devices",
+        "✓ Managed complex state using Redux and React hooks, resulting in a 30% reduction in code complexity",
+        "✓ Integrated with back-end APIs using Axios and Fetch, allowing for dynamic data retrieval and updates in real-time",
+        "✓ Optimized performance by implementing lazy loading and code splitting, as well as using Framer Motion for animation, resulting in a 20% reduction in page load times",
+        "✓ Collaborated with cross-functional teams to deliver projects on time and to required standards, utilizing HTML, CSS, JavaScript, TypeScript, React.js, and Next.js, resulting in a 100% success rate in project delivery",
+      ],
+    },
+
+    {
+      name: "Dream Legacy",
+      location: "Toronto,Canada",
+      startDate: "2020",
+      endDate: "2020 3 month contract",
+      description: [
+        "✓ Developing code in accordance with appropriate design and architectural rules",
+        "✓ Documenting, testing, deployment, maintenance, support of platforms, applications, databases and solutions",
+        "✓ Help and training of other colleagues",
+        "✓ Others tasks connected to the nature of the job",
+        "✓ Code Improvements and Refactoring ",
+        "✓ Best Practices and Rules Maintainer",
+      ],
+    },
+  ],
+
+  education: [
+    {
+      name: "Algonquin College Institute of Technology",
+      location: "Ottawa,Ontario,Canada",
+      startDate: "2020",
+      endDate: "2022",
+      description: ["Associates in Computer Science"],
+    },
+  ],
+
+  socialMedia: {
+    github: "https://github.com/codeboysummer",
+    email: "aw.wahid13@gmail.com",
+    linkedin: "https://www.linkedin.com/in/abdul-wahid-a520b1194/",
+  },
+
+  projects: [
+    {
+      title: "HotTakes",
+      isFeatured: true,
+      thumbnail: "/assets/images/hottakes.png",
+      githubUrl: "https://github.com/codeboysummer/HotTakes",
+      liveUrl: "https://hottakes-f81e8.web.app/",
+    },
+    {
+      title: "One Digital",
+      isFeatured: true,
+      thumbnail: "/assets/images/one.png",
+      githubUrl: "https://github.com/codeboysummer/onedigital",
+      liveUrl: "https://sage-kataifi-c5b8c1.netlify.app/",
+    },
+    {
+      title: "ChangeBase",
+      isFeatured: true,
+      thumbnail: "/assets/images/change.png",
+      githubUrl: "https://github.com/codeboysummer/ChangeBase",
+      liveUrl: "https://changebase-9f826.web.app/",
+    },
+  ],
+};
